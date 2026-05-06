a0358567f09586f54dc2c2ab5eed4b4775075514
Ridho8390 <muhamadridhoabdilah391@gmail.com>
2026-05-06 09:14:21 +0700

    fix conflict

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,16 +1,9 @@
 <?php
 // Mendefinisikan konstanta koneksi
-<<<<<<< HEAD
 define('DB_HOST', 'localhost');      // Server database
 define('DB_USER', 'root');           // Username
 define('DB_PASS', '');               // Password
 define('DB_NAME', 'sistem_absensi_siswa'); // Nama database
-=======
-define('DB_HOST', 'localhost'); // Server database
-define('DB_USER', 'root'); // Username
-define('DB_PASS', ''); // Password
-define('DB_NAME', 'db_pendataan_siswa'); // Nama database
->>>>>>> 9f1a7be1ba9fbb8f0c010c281f0d06c58d93a67a
 
 // Membuat koneksi
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
